0dfc5d962a0535f51b165f9b96c462422bf0b06d
Jackson <42272058+Graydenn@users.noreply.github.com>
2023-10-04 22:30:24 -0400

    redesign

---
+++ datgui.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - dat.gui@0.7.7
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/dat.gui
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/dat.gui@v0.7.7-AK28L9ZRqD4Y8MpY6pDt/mode=imports/optimized/dat.gui.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/dat.gui@v0.7.7-AK28L9ZRqD4Y8MpY6pDt/mode=imports,min/optimized/dat.gui.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/dat.gui@v0.7.7-AK28L9ZRqD4Y8MpY6pDt/dist=es2019,mode=imports/optimized/dat.gui.js';
+export {default} from '/-/dat.gui@v0.7.7-AK28L9ZRqD4Y8MpY6pDt/dist=es2019,mode=imports/optimized/dat.gui.js';
